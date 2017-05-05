df85d1e02706302943301d4fa4035a66d37304e2
runicelf <perovrsq@gmail.com>
2017-05-05 08:55:20 +0300

    space added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends Component{
             <footer className='footer'>
                 <div className='footer__content'>
                     <div className='footer__copy'>&copy;2016 Ипотечный калькулятор. Любое копирование информации запрещено. 
-                        <a className='footer__copy__a' href='#'>Privacy.</a>
+                        <a className='footer__copy__a' href='#'> Privacy.</a>
                     </div>
                     <div className='footer__social-network-logos'>
                         <a href='#' className='footer__social-network-logos__vk'></a>
